695f0b253d4094f818ddca2f6a049e2e08cff777
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2022-03-01 18:12:31 -0300

    Directly fix a problem at its root

--- src/main/java/dev/lostluma/lightningpodoboo/mixin/FireBlockMixin.java
+++ src/main/java/dev/lostluma/lightningpodoboo/mixin/FireBlockMixin.java
@@ -2,6 +2,7 @@ package dev.lostluma.lightningpodoboo.mixin;
 
 import java.util.Random;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,25 +10,31 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import dev.lostluma.lightningpodoboo.CosmeticFireBlock;
 import dev.lostluma.lightningpodoboo.LightningPodoboo;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FireBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 
 @Mixin(FireBlock.class)
 public abstract class FireBlockMixin {
-    @Shadow
-    abstract boolean areBlocksAroundFlammable(BlockView world, BlockPos pos);
+    @Shadow @Final
+    private Object2IntMap<Block> burnChances;
 
     @Redirect(
-        method = "scheduledTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FireBlock;areBlocksAroundFlammable(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z")
+        method = "getBurnChance(Lnet/minecraft/block/BlockState;)I",
+        at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2IntMap;getInt(Ljava/lang/Object;)I")
     )
-    private boolean scheduledTickAreBlocksAroundFlammable(FireBlock fireBlock, BlockView world, BlockPos pos) {
-        return ((FireBlockMixin)(Object)Blocks.FIRE).areBlocksAroundFlammable(world, pos);
+    private int useFireBlocksFlammableMaps(Object2IntMap<Block> burnChances, Object object) {
+        if ((Object)this instanceof CosmeticFireBlock) {
+            return ((FireBlockMixin)(Object)Blocks.FIRE).burnChances.getInt(object);
+        }
+
+        return burnChances.getInt(object);
     }
 
     @Inject(
